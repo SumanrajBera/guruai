152320d72011cfa04acfc38b2f5cbe6898e4216f
SumanrajBera <srbera17@gmail.com>
2026-06-01 11:04:55 +0530

    IPv6 issue

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -2,15 +2,20 @@ import nodemailer from 'nodemailer'
 import { config } from '../config/config.js'
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: config.GOOGLE_USER,
         pass: config.GOOGLE_APP_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
+    },
+    socket: {
+        family: 4
     }
-});
+})
 
 try {
     await transporter.verify()
